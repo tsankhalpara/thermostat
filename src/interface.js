a2b657801e5766bdbd64d41750eabfefcd60fc43
Tara Sankhalpara <tsankhalpara@mac.com>
2019-05-03 12:20:20 +0100

    change jquery interface to link with up and down buttons

--- src/interface.js
+++ src/interface.js
@@ -1,34 +1,23 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
-  $('#temperature').text(thermostat.currentTemperature());
-})
+  $('#temperature').text(thermostat.temperature);
 
-$(document).ready(function() {
-  var thermostat = new Thermostat();
-  $('#power_saving_on').text(thermostat.powerSavingOn());
-})
+  $('#on').text(thermostat.powerSavingOn());
 
-$(document).ready(function() {
-  var thermostat = new Thermostat();
-  $('#power_saving_off').text(thermostat.powerSavingOff());
-})
+  $('#off').text(thermostat.powerSavingOff());
 
-$(document).ready(function() {
-  var thermostat = new Thermostat();
   $('#usage').text(thermostat.usage());
-})
 
-$(document).ready(function() {
-  var thermostat = new Thermostat();
   $('#reset').text(thermostat.reset());
-})
 
-$(document).ready(function() {
-  var thermostat = new Thermostat();
-  $('#up').text(thermostat.up());
-})
+  $('#up').on('mouseup', function() {
+    thermostat.up();
+    $('#temperature').text(thermostat.temperature);
+  })
 
-$(document).ready(function() {
-  var thermostat = new Thermostat();
-  $('#down').text(thermostat.down());
+
+  $('#down').on('mouseup', function() {
+    thermostat.down();
+    $('#temperature').text(thermostat.temperature);
+  })
 })
